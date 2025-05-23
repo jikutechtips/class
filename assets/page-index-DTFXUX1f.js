c3ce062062b149465c3e5a4f4336012779abf88b
jikutechtips <jikutechtips@gmail.com>
2025-05-23 16:34:59 +0300

    Updates

---
+++ assets/page-index-DTFXUX1f.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-BrzHSOkp.js";import{D as a}from"./page-dashboard-BsZqo3A7.js";function t(){return r.jsxs(r.Fragment,{children:[" ",r.jsx(a,{})]})}export{t as D};
--- assets/page-index-DlEDymxz.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./vendor-BrzHSOkp.js";import{D as a}from"./page-dashboard-LNqkcw5r.js";function t(){return r.jsxs(r.Fragment,{children:[" ",r.jsx(a,{})]})}export{t as D};
